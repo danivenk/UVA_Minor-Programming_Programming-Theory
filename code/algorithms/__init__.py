e90892ffa5ae4c98f46c5641e12cf55632be4af8
danivenk <danivenk@gmail.com>
2020-06-19 16:52:48 +0200

    forgot to change algorithms/__init__.py

--- code/algorithms/__init__.py
+++ code/algorithms/__init__.py
@@ -1,2 +1,4 @@
 from .random import Random_Connections
-from .random_relax import Random_Relax_Connections
+from .greedy import Greedy
+from .astar import A_star
+from .hill_climber import Hill_Climber
