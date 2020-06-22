4f356d65a72946de6c4b3c6f1fe5a9af6eae4fb5
= <=>
2020-06-22 14:00:05 +0200

    improved performance/style

--- code/algorithms/astar.py
+++ code/algorithms/astar.py
@@ -23,14 +23,20 @@ class A_Star():
         result              - result or list of results of the algorithm;
         choices             - dictionary containing all starting points;
         number_of_results   - number of results that will be returned;
+        speed               - minimal time in minutes per km;
 
     property:
         result              - returns result or list of results;
+        speed               - returns speed as a float number;
 
     methods:
+        update_result       - updates result according to number_of_results;
         time_per_km         - returns lowest duration divided by distance;
         station_distance    - returns distance between two stations in km;
-        create_line          - returns shortest line(s) between two stations;
+        copy_connections    - copies connections from one line to another;
+        add_choice          - adds choice to choices;
+        new_line            - creates new line and adds to choices;
+        create_line         - returns shortest line(s) between two stations;
     """
 
     def __init__(self, connections, max_duration, number_of_results=1):
@@ -48,6 +54,7 @@ class A_Star():
         self._result = []
         self._choices = defaultdict(float)
         self._number_of_results = number_of_results
+        self._speed = self.time_per_km()
 
     @property
     def result(self):
@@ -56,6 +63,24 @@ class A_Star():
     @result.setter
     def result(self, value):
         self._result = value
+
+    def update_result(self, best_option):
+        """
+        Update result according the maximum number of results
+
+        parameters:
+            best_option    - line that is best option from A to B;
+        """
+        if self._number_of_results == 1:
+            self.result = best_option
+
+        else:
+            # Add best line to results for number_of_results times
+            self._result.append(best_option)
+
+    @property
+    def speed(self):
+        return self._speed
 
     def time_per_km(self):
         """
@@ -93,6 +118,70 @@ class A_Star():
 
         return distance.distance(station1.position, station2.position).km
 
+    def copy_connections(self, new_line, old_line):
+        """
+        Copy all connections from one line to the other
+
+        parameters:
+            new_line    - line where the connections will be added to;
+            old_line    - line where the connections will be copied from;
+        """
+        for old_connection in old_line.connections:
+            new_line.add_connection(old_connection,
+                                    self._max_duration)
+
+    def add_choice(self, line, station2):
+        """
+        Calculates penalty time and adds line to choices
+
+        parameters:
+            line        - line that will be added to choices
+            station2    - the end station where the line needs to go to
+        """
+
+        # Add minimal duration to get to station2
+        line.penalty = self.station_distance(
+                        line.stations[-1], station2) * self.speed
+
+        # Add line to choices with sum of duration and
+        # minimal duration if less than max duration
+        if line.duration + line.penalty <= self._max_duration:
+            self._choices[line] = line.duration + line.penalty
+
+    def new_line(self, connection, line, best_option, station2):
+        """
+        Creates copy of best_option, add new connection and add to choices
+
+        parameters:
+            connection  - connection that is going to be added;
+            line        - line that will be updated;
+            best_option - old line that will be copied;
+            station2    - end station line is going to;
+        """
+
+        # If there are connections in best_option
+        if best_option.connections:
+
+            # If connection is not same as last connection of best option
+            if connection != best_option.connections[-1]:
+
+                # Copy all connections from best_option to line
+                self.copy_connections(line, best_option)
+
+                # Add new connection
+                line.add_connection(connection, self._max_duration)
+
+                # Add line to choices
+                self.add_choice(line, station2)
+
+        else:
+
+            # Add new connection
+            line.add_connection(connection, self._max_duration)
+
+            # Add line to choices
+            self.add_choice(line, station2)
+
     def create_line(self, station1, station2):
         """
         Returns line(s) between two stations with the shortest duration
@@ -108,7 +197,7 @@ class A_Star():
         uid = 0
 
         # Set variables
-        speed = self.time_per_km()
+        speed = self._speed
         total_duration = self.station_distance(station1, station2) * speed
 
         # If total_duration is shorter or equal to max_duration, run function
@@ -127,25 +216,18 @@ class A_Star():
                 # If it an empty line add first connection
                 if uid == 0:
                     station = station1
-                    start = True
 
                 else:
                     station = best_option.stations[-1]
-                    start = False
 
                 # If the last station in the best line is the same as station2
                 if station == station2:
+                    self.update_result(best_option)
 
-                    if self._number_of_results == 1:
-                        self.result = best_option
+                    # Add one to counter to get to desired number of results
+                    c += 1
+                    if c == self._number_of_results:
                         break
-
-                    else:
-                        # Add best line to results for number_of_results times
-                        self._result.append(best_option)
-                        c += 1
-                        if c == self._number_of_results:
-                            break
 
                 else:
 
@@ -154,22 +236,7 @@ class A_Star():
                         uid += 1
                         line = Line(uid, station1)
 
-                        if not start:
-
-                            for old_connection in best_option.connections:
-                                line.add_connection(old_connection,
-                                                    self._max_duration)
-
-                        line.add_connection(connection, self._max_duration)
-
-                        # Add minimal duration to get to station2
-                        line.penalty = (self.station_distance(
-                            connection.other(station), station2) * speed)
-
-                        # Add line to choices with sum of duration and
-                        # minimal duration if less than max duration
-                        if line.duration + line.penalty <= self._max_duration:
-                            self._choices[line] = line.duration + line.penalty
+                        self.new_line(connection, line, best_option, station2)
 
                 # Delete the line used for calculations from options
                 del self._choices[best_option]
